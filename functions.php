2661b684af1bbde9e18344624c5c79e8ea210bd7
Sanish <sanish_shr1995@hotmail.com>
2020-06-10 09:57:22 +0545

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,79 @@
+<?php 
+/**
+ * MyFirstTheme's functions and definitions
+ *
+ * @package MyFirstTheme
+ * @since MyFirstTheme 1.0
+ */
+ 
+/**
+ * First, let's set the maximum content width based on the theme's design and stylesheet.
+ * This will limit the width of all uploaded images and embeds.
+ */
+if ( ! isset( $content_width ) )
+    $content_width = 800; /* pixels */
+ 
+if ( ! function_exists( 'myfirsttheme_setup' ) ) :
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ */
+function myfirsttheme_setup() {
+ 
+    /**
+     * Make theme available for translation.
+     * Translations can be placed in the /languages/ directory.
+     */
+    load_theme_textdomain( 'myfirsttheme', get_template_directory() . '/languages' );
+ 
+    /**
+     * Add default posts and comments RSS feed links to <head>.
+     */
+    add_theme_support( 'automatic-feed-links' );
+ 
+    /**
+     * Enable support for post thumbnails and featured images.
+     */
+    add_theme_support( 'post-thumbnails' );
+ 
+    /**
+     * Add support for two custom navigation menus.
+     */
+    register_nav_menus( array(
+        'primary'   => __( 'Primary Menu', 'myfirsttheme' ),
+        'secondary' => __('Secondary Menu', 'myfirsttheme' )
+    ) );
+ 
+    /**
+     * Enable support for the following post formats:
+     * aside, gallery, quote, image, and video
+     */
+    add_theme_support( 'post-formats', array ( 'aside', 'gallery', 'quote', 'image', 'video' ) );
+}
+endif; // myfirsttheme_setup
+add_action( 'after_setup_theme', 'myfirsttheme_setup' );
+
+function add_theme_scripts() {
+    //css
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+   
+    //Js files
+    wp_enqueue_script( 'jquery.min', get_template_directory_uri() . './assets/js/vendor/jquery.min.js');
+    wp_enqueue_script( 'popper.min', get_template_directory_uri() . './assets/js/vendor/popper.min.js');
+    wp_enqueue_script( 'bootstrap.min', get_template_directory_uri() . './assets/js/vendor/bootstrap.min.js');
+    wp_enqueue_script( 'functions', get_template_directory_uri() . './assets/js/functions.js');
+
+   
+    wp_enqueue_script( 'main', get_template_directory_uri() . './assets/js/main.js');
+   
+      if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+      }
+  }
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+
+?>
